507fdd9f052bc66e53922c8a46a9c76e3d2217f4
wxx <614610440@qq.com>
2019-11-14 18:00:03 +0800

    add logon

--- controllers/logon.go
+++ controllers/logon.go
@@ -2,8 +2,10 @@ package controllers
 
 import (
 	models "NewWorld/models"
+	"fmt"
 
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
 )
 
 // LogonController .
@@ -13,18 +15,26 @@ type LogonController struct {
 
 // Get .
 func (l *LogonController) Get() {
-
+	logs.Info("LogonController Get()")
+	l.TplName = "logon.html"
 }
 
 // Post .
 func (l *LogonController) Post() {
+	logs.Info("user logon!")
 	name := l.GetString("name")
 	ID := l.GetString("user_id")
 	pwd := l.GetString("pwd")
-	// image := l.GetString("image")
-	image := ""
+	fmt.Println(name, ID, pwd)
+	image := l.GetString("image")
 
 	addStatus := models.GetInstance().AddUser(ID, name, pwd, image)
 
-	l.Ctx.ResponseWriter.WriteHeader(int(addStatus))
+	fmt.Println(addStatus)
+	if addStatus == models.HAVEEXIST {
+		l.Ctx.ResponseWriter.WriteHeader(204)
+	} else {
+		l.Ctx.ResponseWriter.WriteHeader(200)
+	}
+	l.ServeJSON()
 }
---
+++ static/js/logon.js
@@ -0,0 +1,62 @@
+﻿function pwdCheck() {
+
+	if (pwd.value == ensure_pwd.value) {
+		help_inline.style.display = "none";
+		return true;
+	} else {
+		help_inline.style.display = "";
+		return false;
+	}
+};
+
+function logonCheck() {
+	if (email.value == "") {
+		alert("邮箱不能为空!");
+		return false;
+	}
+
+	if (user_id.value == "") {
+		alert("账户不能为空!");
+		return false;
+	}
+
+	if (user_name.value == "") {
+		alert("昵称不能为空!");
+		return false;
+	}
+
+	if (pwd.value == "") {
+		alert("密码不能为空!");
+		return false;
+	}
+
+	if (pwd.value != ensure_pwd.value) {
+		alert("确认密码和密码不同!");
+		return false;
+	}
+
+	return true;
+};
+
+function logon() {
+	if (logonCheck() == true) {
+		jQuery.ajax({
+			type: "POST",
+			async: false,
+			dataType: "json",
+			url: "/logon",
+			data: $('#user_form').serialize(),
+			statusCode: {
+				200:function(){
+					alert("注册成功!");
+				},
+				204:function(){
+					alert("用户ID已存在!");
+				},
+			},
+			error: function(result) {
+				alert("注册异常, 请稍候再试!");
+			}
+		});
+	}
+}
